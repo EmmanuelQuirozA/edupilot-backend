655b4499acff949ed1d4d8b480fac54b382efcf7
Emmanuel Quiroz <eqa@outlook.com>
2025-07-01 23:11:48 -0600

    Fixed Cors for production

--- src/main/java/com/monarchsolutions/sms/config/CorsConfig.java
+++ src/main/java/com/monarchsolutions/sms/config/CorsConfig.java
@@ -15,7 +15,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000","http://localhost:3001", "http://localhost:5173/")
+                    .allowedOrigins(
+                    "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://localhost:5173/",
+                        "https://test.edupilot.com.mx",
+                        "https://edupilot.com.mx"
+                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
