55fe4b3fe8fd7ac2dac49ef4b67f4a22860604cb
Emmanuel Quiroz <eqa@outlook.com>
2025-07-20 22:22:06 -0600

    "self-details" error fix 3

--- src/main/java/com/monarchsolutions/sms/controller/UserController.java
+++ src/main/java/com/monarchsolutions/sms/controller/UserController.java
@@ -208,7 +208,7 @@ public class UserController {
 
 	// Endpoint for retrieving the user details
 	@PreAuthorize("hasAnyRole('ADMIN','SCHOOL_ADMIN')")
-	@GetMapping("/{userId:\\d+}")
+	@GetMapping("/{userId:[0-9]+}")
 	public ResponseEntity<?> getUser(
 		@RequestHeader("Authorization") String authHeader,
 		@PathVariable("userId") Long userId,
