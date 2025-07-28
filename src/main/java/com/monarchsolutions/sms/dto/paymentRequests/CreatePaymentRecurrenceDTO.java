65515c26b4d7f14eace4ee10ba28d3d2a4507900
EmmanuelQuirozA <128554601+EmmanuelQuirozA@users.noreply.github.com>
2025-07-27 23:16:09 -0600

    Add endpoint for payment recurrence

---
+++ src/main/java/com/monarchsolutions/sms/dto/paymentRequests/CreatePaymentRecurrenceDTO.java
@@ -0,0 +1,92 @@
+package com.monarchsolutions.sms.dto.paymentRequests;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CreatePaymentRecurrenceDTO {
+    private Integer    student_id;
+    private Integer    payment_concept_id;
+    private BigDecimal amount;
+    private String     fee_type;
+    private BigDecimal late_fee;
+    private Integer    late_fee_frequency;
+    private String     period;
+    private Integer    interval_count;
+    private LocalDate  start_date;
+    private LocalDate  end_date;
+    private String     comments;
+    private LocalDate  payment_month;
+
+    public Integer getStudent_id() {
+        return student_id;
+    }
+    public void setStudent_id(Integer student_id) {
+        this.student_id = student_id;
+    }
+    public Integer getPayment_concept_id() {
+        return payment_concept_id;
+    }
+    public void setPayment_concept_id(Integer payment_concept_id) {
+        this.payment_concept_id = payment_concept_id;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public String getFee_type() {
+        return fee_type;
+    }
+    public void setFee_type(String fee_type) {
+        this.fee_type = fee_type;
+    }
+    public BigDecimal getLate_fee() {
+        return late_fee;
+    }
+    public void setLate_fee(BigDecimal late_fee) {
+        this.late_fee = late_fee;
+    }
+    public Integer getLate_fee_frequency() {
+        return late_fee_frequency;
+    }
+    public void setLate_fee_frequency(Integer late_fee_frequency) {
+        this.late_fee_frequency = late_fee_frequency;
+    }
+    public String getPeriod() {
+        return period;
+    }
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+    public Integer getInterval_count() {
+        return interval_count;
+    }
+    public void setInterval_count(Integer interval_count) {
+        this.interval_count = interval_count;
+    }
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    public LocalDate getPayment_month() {
+        return payment_month;
+    }
+    public void setPayment_month(LocalDate payment_month) {
+        this.payment_month = payment_month;
+    }
+}
